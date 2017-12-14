e2b6dc0076240ddb8fefd4e3eb97b88b09521bd5
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2017-12-14 12:59:45 +0530

    Update __manifest__.py

--- vtiger_connector_partner/__manifest__.py
+++ vtiger_connector_partner/__manifest__.py
@@ -5,7 +5,7 @@
     'summary': 'Partner VTiger Integration',
     'description': """
         Partner VTiger Integration""",
-    'author': 'Serpent Consulting Services Pvt Ltd',
+    'author': 'Serpent Consulting Services Pvt. Ltd.',
     'website': 'https://www.serpentcs.com',
     'category': '',
     'depends': [
