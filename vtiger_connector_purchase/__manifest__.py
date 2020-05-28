93b9f88ec00a41c1a4bc3ec064f83b85365d97bc
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-05-28 11:02:15 +0530

    Update __manifest__.py

--- vtiger_connector_purchase/__manifest__.py
+++ vtiger_connector_purchase/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'VTiger Purchase Connector',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Purchases',
     'license': 'AGPL-3',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
