610824c55c3e841df782a9d8dbff74c20dae7b3b
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-05-28 11:00:32 +0530

    Update __manifest__.py

--- vtiger_connector_calendar/__manifest__.py
+++ vtiger_connector_calendar/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'VTiger Calender Connector',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Marketing',
     'license': 'AGPL-3',
     'author': 'Serpent Consulting Services Pvt. Ltd.',
