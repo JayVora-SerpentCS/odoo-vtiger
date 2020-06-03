18c9fb44b0899da621704cd6a5d7c23e36724f3b
Serpent Consulting Services Pvt. Ltd <serpentcs.lp@gmail.com>
2020-06-03 13:08:27 +0530

    Update __manifest__.py

--- vtiger_connector_products/__manifest__.py
+++ vtiger_connector_products/__manifest__.py
@@ -13,5 +13,5 @@
     'data': ['views/res_company_view.xml',
              'views/product_template_view.xml'],
     'installable': True,
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
 }
